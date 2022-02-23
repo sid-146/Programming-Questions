d970ee2a2e6b29fc647245ccc692b1f86c789479
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-02-23 23:52:02 +0530

    Added files

--- FiboncciRecursion.cpp
+++ FiboncciRecursion.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
+void fibonacci(int num, int sum, int first, int second)
+{
+    if (num > 0)
+    {
+        cout << first << " ";
+        int sum = first + second;
+        first = second;
+        second = sum;
+        return fibonacci(num - 1, sum, first, second);
+    }
+}
+
 int main()
 {
+    fibonacci(10, 0, 0, 1);
     return 0;
 }
