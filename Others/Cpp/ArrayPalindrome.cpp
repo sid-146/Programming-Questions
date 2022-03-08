3aab19202d7fb39f5dbb062bcb2c24a52bc0010e
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-03-08 23:42:53 +0530

    Create ArrayPalindrome.cpp

---
+++ Others/Cpp/ArrayPalindrome.cpp
@@ -0,0 +1,32 @@
+// ! Find minimum number of merge operations to make array palindrome
+
+/*
+Given a list of non-negative integers, find the minimum number of merge operations
+to make a palindrome. A merge operation can only be performed on two adjacent elements
+The result of a merge operation is that the two adjacent elements are replaced with their sum.
+
+Testcase:
+    input -> [6,1,3,7]
+    output -> 1
+
+    explanation : merge index 0 and 1 i.e [6, 1] = [7,3,7]
+
+    [6,1,4,3,1,7]
+    ans = 2
+    merge index 0,1 and 3, 4
+    [7,4,4,7]
+
+    [1,3,3,1]
+    ans = 0
+    already palindrome
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
