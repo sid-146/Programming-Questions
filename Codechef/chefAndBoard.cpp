9769b5371a68eb9e7acbb0b1229b79857aa44b10
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-06-23 16:25:11 +0530

    main

---
+++ Codechef/chefAndBoard.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int testcase;
+
+    while (testcase)
+    {
+        int n;
+        cin >> n;
+
+        for (int i = 0; i < n;)
+        {
+        }
+
+        testcase--;
+    }
+
+    return 0;
+}
